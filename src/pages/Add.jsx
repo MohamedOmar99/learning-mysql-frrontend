f9e701475cdae34cd6e41a141c7d1ffc014e7127
--add <--add>
2023-12-06 12:42:35 +0300

    Finishing

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Add = () => {
-  return <div>Add</div>;
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("http://localhost:8800/books", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(book),
+      });
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="form">
+      <h1>Add new book</h1>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+        name="title"
+      />
+      <input
+        type="text"
+        placeholder="desc"
+        onChange={handleChange}
+        name="desc"
+      />
+      <input
+        type="number"
+        placeholder="price"
+        onChange={handleChange}
+        name="price"
+      />
+      <input
+        type="text"
+        placeholder="cover"
+        onChange={handleChange}
+        name="cover"
+      />
+      <button onClick={handleClick}>Add</button>
+    </div>
+  );
 };
 
 export default Add;
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,22 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Books = () => {
   const [books, setBooks] = useState([]);
-  useState(() => {
+
+  useEffect(() => {
     const fetchBooks = async () => {
       try {
         const res = await fetch("http://localhost:8800/books");
         const data = await res.json();
         setBooks(data);
         console.log(data);
-        console.log(books);
       } catch (err) {
         console.log(err);
       }
     };
+
     fetchBooks();
   }, []);
-  return <div>Books</div>;
+
+  const handleDelete = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:8800/books/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <h1>My library</h1>
+      <div className="books">
+        {books.map((book, i) => (
+          <div className="book" key={i}>
+            {book.cover && <img src={book.cover} alt="" />}
+            <h1>{book.title}</h1>
+            <p>{book.desc}</p>
+            <p>{book.price}</p>
+            <button onClick={() => handleDelete(book.id)} className="delete">
+              Delete
+            </button>
+            <button className="Update">
+              <Link to={`/update/${book.id}`}>Update</Link>
+            </button>
+          </div>
+        ))}
+      </div>
+      <Link to={"/add"} className="addHome">
+        Add books
+      </Link>
+    </>
+  );
 };
 
 export default Books;
